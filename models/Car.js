24fe6eaf443c45e126f687098b813155459347ae
Oskar Eriksson <oskar.eriksson.96@hotmail.com>
2017-05-17 14:10:56 +0200

    Started working on a basic Car model. Also added a post and get route for the cars.
    Added a User.js that will be used for user registration later.
    
    Updated .gitignore and package.json also.

---
+++ models/Car.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const Car = new Schema({
+  brand: {type: String, required: [true, "You need to add the brand of the car."]},
+  transmission: {type: String, required: [true, "You need to select manual/automatic transmission."]},
+  seats: {type: Number, min: 2, max: 7, required: [true, "You need to enter the amount of seats in the car."]},
+  roofRack: {type: Boolean, required: [true, "Does the car have a roofrack available? True or false."]},
+  towbar: {type: Boolean, required: [true, "Does the car have a towbar available? True or false."]},
+  pricePerDay: {type: Number, min: 500, max: 2000, required: [true, "You need to enter the daily price of renting the car."]}
+});
+
+module.exports = mongoose.model("Car", Car);
--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,20 @@
 const router = require("express").Router();
 const mongodb = require("mongodb");
+const Car = require("../models/Car.js");
+
+router.get("/cars", (req, res) => {
+  Car.find({}, (error, result) => {
+    res.json(result);
+  });
+});
+
+router.post("/addcar", (req, res) => {
+  let car = new Car(req.body);
+
+  car.save((error, result) => {
+    if(error) res.send(error);
+    res.json(result);
+  });
+});
 
 module.exports = router;
