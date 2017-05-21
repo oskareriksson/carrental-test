d84b8dc5c600173106addae8a7ef5de7135238e2
Oskar Eriksson <oskar.eriksson.96@hotmail.com>
2017-05-21 12:46:33 +0200

    Fixed a typo in app.js and added passport-local-mongoose plugin to User.js model

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(session({
 
 app.use(passport.initialize);
 app.use(passport.session);
-passport.user(User.createStrategy());
+passport.use(User.createStrategy());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
--- models/User.js
+++ models/User.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const User = new Schema({
-  firstName: {type: String, required: [true, "You need to enter a firstname."]},
-  lastName: {type: String, required: [true, "You need to enter a lastname."]},
   email: {type: String, required: [true, "You need to enter an email adress."]},
   password: {type: String, required: [true, "A password is required to login."]}
 });
+
+User.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model("User", User);
